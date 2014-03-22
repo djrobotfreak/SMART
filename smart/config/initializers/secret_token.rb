798274f31bda4254de6cf552a309230eb133c1e5
Devan Huapaya <huapayadevan@gmail.com>
2014-03-22 14:43:14 -0500

    Rails app created
    Need to run 'bundle install' without quotes.

---
+++ smart/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smart::Application.config.secret_key_base = '0d35fdc9ed8dcab26a3ed3959f485554d4b2fff9753af311a0e79ddf7b21b87684b35e693afd07e3a584b2e62daaaf4195779b9f3b89143d90277b78353be16f'
